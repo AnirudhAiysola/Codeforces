2361830d8bec7574c3b4b6e6292b93892c8486f5
anirudhaiysola <anirudhisola1011@gmail.com>
2024-04-10 19:36:06 +0530

    Problem-734A

---
+++ 800/AntonAndDanik.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    string s;
+    cin >> n >> s;
+    int c1 = 0, c2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == 'A')
+        {
+            c1++;
+        }
+        else
+        {
+            c2++;
+        }
+    }
+    if (c1 > c2)
+    {
+        cout << "Anton" << endl;
+    }
+    else if (c1 < c2)
+    {
+        cout << "Danik" << endl;
+    }
+    else
+    {
+        cout << "Friendship" << endl;
+    }
+    return 0;
+}
