956c1e9ac16ccc8920ab14bacc7b413d982cb965
anirudhaiysola <anirudhisola1011@gmail.com>
2024-04-17 12:01:59 +0530

    Problem-96A

---
+++ 900/Football.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    bool flag = false;
+    for (int i = 0; i < str.size() - 1; i++)
+    {
+        int count = 1;
+        while (i < str.size() - 1 && str[i] == str[i + 1])
+        {
+            i++;
+            count++;
+        }
+        if (count == 7)
+        {
+            flag = true;
+            break;
+        }
+    }
+    if (flag)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
